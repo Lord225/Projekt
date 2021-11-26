002b113afc72d46b796d980ff41e755c62c1c2d9
Lord225 <zlotymaciej@gmail.com>
2021-11-26 14:24:49 +0100

    merging

---
+++ src/PAD.cpp
@@ -0,0 +1,73 @@
+#include <Arduino.h>
+
+using namespace std;
+
+class PAD
+{
+private:
+    int X_pin = NULL;
+    int Y_pin = NULL;
+    int SW_pin = NULL;
+    int x = analogRead(X_pin);
+    int y = analogRead(Y_pin);
+    int sw = digitalRead(SW_pin);
+
+public:
+    void set_PAD(int x, int y, int SW)
+    {
+        X_pin = x;
+        Y_pin = y;
+        SW_pin = SW;
+        pinMode(SW_pin, INPUT_PULLUP);
+        digitalWrite(SW_pin, LOW);
+    };
+    void update()
+    {
+        x = analogRead(X_pin);
+        y = analogRead(Y_pin);
+        sw = digitalRead(SW_pin);
+    };
+    string position(int druk)
+    {
+        update();
+        if (x >= y && x <= -y + 1023)
+        {
+            Serial.println("UP");
+            check();
+            return "UP";
+        }
+        if (x <= y && x >= -y + 1023)
+        {
+
+            Serial.println("DOWN");
+            check();
+            return "DOWN";
+        }
+        if (x < y && x < -y + 1023)
+        {
+
+            Serial.println("LEFT");
+            check();
+            return "LEFT";
+        }
+        if (x > y && x > -y + 1023)
+        {
+
+            Serial.println("RIGHT");
+            check();
+            return "RIGHT";
+        }
+    }
+    void check()
+    {
+        Serial.print("x,y - ");
+        Serial.print(x);
+        Serial.print(",");
+        Serial.print(y);
+        Serial.print("\n");
+        Serial.print("SW - ");
+        Serial.print(digitalRead(SW_pin));
+        Serial.print("\n");
+        delay(1000);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-<<<<<<< HEAD
 #include <iostream>
 #include "display/Display.h"
 #include "app.h" 
@@ -14,28 +13,22 @@ DisplayExternal displ = DisplayExternal((uint8_t)13, (uint8_t)11, (uint8_t)10, M
 #endif
 
 Application app = Application(displ);
-=======
 #include "PAD.cpp"
 #include <cstdlib>
 
 PAD pad1;
->>>>>>> 174bc746969bf217c9f3ca70ce54eaae53e07969
 
 void setup()
 {
     pad1.set_PAD(0, 1, 4);
     Serial.begin(38400);
-<<<<<<< HEAD
     displ.init();
     app.on_start();
-=======
     String lolek;
->>>>>>> 174bc746969bf217c9f3ca70ce54eaae53e07969
 }
 
 void loop()
 {
-<<<<<<< HEAD
     const uint32_t start = millis();
 
     app.on_update();
@@ -46,9 +39,7 @@ void loop()
     const uint32_t delta = start - end;
 
     delay(max((uint32_t)1, (uint32_t)TIME_PER_FRAME_MS));
-}
-=======
+    
     pad1.position(1);
     delay(1000);
 }
->>>>>>> 174bc746969bf217c9f3ca70ce54eaae53e07969
